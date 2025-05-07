d232f8e34941f7625ba34931d4fde1fff405027b
Pranjali Hajare <pranjalihajare2002@gmail.com>
2025-05-07 20:46:48 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    domains: ['cadn.imagin.studio']
+  }
 };
 
 export default nextConfig;
